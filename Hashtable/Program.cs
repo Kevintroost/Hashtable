c0eb28e8d2d1d1eb659f5ba0fdef90295e76d133
KEVN <troostkevin8@gmail.com>
2025-03-13 14:17:02 +0100

    hashtable

---
+++ Hashtable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+
+Hashtable myHashTable = new Hashtable();
+
+myHashTable["Alice"] = 25;
+myHashTable["Bob"] = 30;
+myHashTable["Charlie"] = 35;
+myHashTable["Henk"] = 40;
+
+Console.WriteLine("Charlie's age: " + (int?)myHashTable["Charlie"]);
+
+
+if (myHashTable.ContainsKey("Alice"))
+{
+    Console.WriteLine("Alice is found, Age: " + (int?)myHashTable["Alice"]);
+}
+
+
+myHashTable.Remove("Bob");
+
+
+Console.WriteLine("\nAll Entries:");
+foreach (DictionaryEntry pair in myHashTable)
+{
+    Console.WriteLine($"{pair.Key}: {pair.Value}");
+}
+
+Console.WriteLine("\nPress any key to exit...");
+Console.ReadKey();
